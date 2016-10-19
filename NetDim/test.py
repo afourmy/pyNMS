7768f0a4020f9b2963f7636d9c8f6b7dcf71f449
minto <minto@hotmail.fr>
2016-10-19 20:42:05 +0200

    remove pyc

--- NetDim/test.py
+++ NetDim/test.py
@@ -7,6 +7,9 @@ import sys
 from inspect import getsourcefile
 from os.path import abspath, pardir, join
 
+# prevent python from writing *.pyc files / __pycache__ folders
+sys.dont_write_bytecode = True
+
 path_app = abspath(getsourcefile(lambda: 0))[:-7]
 
 if path_app not in sys.path:
