41d835f12c051a7965757648b780de26124ef094
minto <minto@hotmail.fr>
2016-11-20 18:20:13 +0100

    pythonic tk for rwa

--- NetDim/optical_networks/rwa_window.py
+++ NetDim/optical_networks/rwa_window.py
@@ -13,48 +13,42 @@ class RWAWindow(FocusTopLevel):
         self.title("Routing and Wavelength Assignment")
                         
         # label frame
-        self.lf_rwa = ttk.Labelframe(self, padding=(6, 6, 12, 12), 
-                                    text='Routing and Wavelength Assignment')
-        self.lf_rwa.grid(column=0, columnspan=2, pady=5, padx=5, 
-                                                        row=1, sticky='nsew')
+        lf_rwa = Labelframe(self)
+        lf_rwa.text = 'Routing and Wavelength Assignment'
                                                         
         ## Graph transformation and scenario name
                 
-        self.sco_name = ttk.Label(self, text="Scenario name :")
-        self.sco_name.grid(in_=self.lf_rwa, row=0, column=0, pady=5, padx=5,
-                                                                    sticky="w")
-        self.entry_sco = ttk.Entry(self, width=20)
-        self.entry_sco.grid(in_=self.lf_rwa, row=0, column=1, pady=5, padx=5,
-                                                                    sticky="e")
+        sco_name = Label(self)
+        sco_name.text = "Scenario name :"
+
+        self.entry_sco = Entry(self, width=20)
         
-        self.button_gt = ttk.Button(self, text="Graph transformation", 
-                                                command=self.transform_graph)
-        self.button_gt.grid(in_=self.lf_rwa, row=1, column=0, columnspan=2, 
-                                        pady=5, padx=5, sticky="nsew")
-        
-        ## RWA algorithm:
-        
+        button_gt = Button(self)
+        button_gt.text = "Graph transformation"
+        button_gt.command = self.transform_graph
+
         # label "algorithm"
-        self.algorithm = ttk.Label(self, text="Algorithm :")
-        self.algorithm.grid(in_=self.lf_rwa, row=2, column=0, pady=5, padx=5,
-                                                                    sticky="w")
+        algorithm = Label(self) 
+        algorithm.text = "Algorithm :"
         
         # combobox for the user to change the RWA algorithm
-        self.var_rwa_type = tk.StringVar()
-        self.rwa_list = ttk.Combobox(self, 
-                                    textvariable=self.var_rwa_type, width=17)
-        self.rwa_list["values"] = (
-                                       "Linear programming", 
-                                       "Largest degree first"
-                                       )
-        self.rwa_list.set("Linear programming")
-        self.rwa_list.grid(in_=self.lf_rwa, row=2, column=1, 
-                                                    pady=5, padx=5, sticky="e")
+        self.rwa_list = Combobox(self, width=17)
+        algorithms = ("Linear programming", "Largest degree first")
+        self.rwa_list["values"] = algorithms
+        self.rwa_list.current(0)
                                                     
-        self.button_run_alg = ttk.Button(self, text="Run algorithm", 
-                                                command=self.run_algorithm)
-        self.button_run_alg.grid(in_=self.lf_rwa, row=3, column=0, columnspan=2, 
-                                        pady=5, padx=5, sticky="nsew")
+        button_run_alg = Button(self)
+        button_run_alg.text = "Run algorithm"
+        button_run_alg.command = self.run_algorithm
+                                        
+        # grid placement
+        lf_rwa.grid(1, 0, 1, 2)
+        sco_name.grid(0, 0, in_=lf_rwa)
+        self.entry_sco.grid(0, 1, sticky="e", in_=lf_rwa)
+        button_gt.grid(1, 0, 1, 2, in_=lf_rwa)
+        algorithm.grid(2, 0, in_=lf_rwa)
+        self.rwa_list.grid(2, 1, sticky="e", in_=lf_rwa)
+        button_run_alg.grid(3, 0, 1, 2, in_=lf_rwa)
         
         # hide the window when closed
         self.protocol("WM_DELETE_WINDOW", self.withdraw)
@@ -66,7 +60,7 @@ class RWAWindow(FocusTopLevel):
         self.ms.cs.ntw.RWA_graph_transformation(name)
 
     def run_algorithm(self):
-        algorithm = self.var_rwa_type.get()
+        algorithm = self.rwa_list.get()
         if algorithm == "Linear programming":
             self.ms.cs.ntw.LP_RWA_formulation()
         else:
