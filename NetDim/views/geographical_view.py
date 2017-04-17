0b9d0d1af5478d94a009b248364109d558440ee0
minto <minto@hotmail.fr>
2017-04-17 21:09:33 +0200

    map update

--- NetDim/views/geographical_view.py
+++ NetDim/views/geographical_view.py
@@ -1,6 +1,6 @@
 # NetDim (contact@netdim.fr)
 
-import re
+import numpy as np
 import warnings
 from os.path import join
 from tkinter import filedialog
@@ -18,9 +18,8 @@ class GeographicalView(BaseView):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
-        # map 
+        # initialize the map 
         self.world_map = Map(self)
-        self.world_map.change_projection('mercator')
         self.world_map.create_meridians()
         self.world_map.center_map([[7, 49]])
             
@@ -42,7 +41,7 @@ class GeographicalView(BaseView):
     def move_sphere(self, event):
         coords = self.world_map.from_points((event.x, event.y), dosphere=1)
         if coords and self.world_map.is_spherical():
-            self.world_map.map_temp['centerof'] = coords
+            self.world_map.center = coords
             self.world_map.change_projection(self.world_map.mode)
                         
     @update_coordinates
@@ -181,27 +180,23 @@ class Map():
 
     delta = 3600.0
     halfX = 648000.0
-    ylimit = 84
     mflood = []
     lonlat = []
     map_temp = {}
     
     def __init__(self, view, viewx=500, viewy=250):
         self.viewx, self.viewy = viewx, viewy
-        # view canvas
         self.cvs = view.cvs
-        # self.map_obj
-        # set of all canvas ids
         self.map_ids = set()
         self.scale = 0.05
-        self.mode = 'linear'
+        self.mode = 'mercator'
         self.change_projection(self.mode)
 
     def scale_map(self, ratio=1, center=True):
         # instead of remembering the center before zooming, it would be better
         # to zoom according to where the mouse is located
-        self.map_temp['center_x'] = (self.cvs.xview()[0] + self.cvs.xview()[1])/2
-        self.map_temp['center_y'] = (self.cvs.yview()[0] + self.cvs.yview()[1])/2
+        self.cx = (self.cvs.xview()[0] + self.cvs.xview()[1])/2
+        self.cy = (self.cvs.yview()[0] + self.cvs.yview()[1])/2
         # calc ratio of current and previous scale
         self.scale *= float(ratio)
         # update scrollregion and scale
@@ -250,7 +245,7 @@ class Map():
                 return
             if len(coords) < 4:
                 coords = coords * 2
-            obj_id = self.cvs.create_line(coords, fill='black', tags=('meridians',))
+            obj_id = self.cvs.create_line(coords, fill='black', tags=('meridians',), smooth=True)
         
     def load_map(self, data):
         for row in data:
@@ -267,15 +262,15 @@ class Map():
             moveY = y/(self.scale_y*self.scale) - (scrollY[1] - scrollY[0])/2.0
         else:
             # visible center
-            moveX = self.map_temp['center_x'] - (scrollX[1] - scrollX[0]) / 2.0
-            moveY = self.map_temp['center_y'] - (scrollY[1] - scrollY[0]) / 2.0
+            moveX = self.cx - (scrollX[1] - scrollX[0]) / 2.0
+            moveY = self.cy - (scrollY[1] - scrollY[0]) / 2.0
         self.cvs.xview('moveto', moveX)
         self.cvs.yview('moveto', moveY)
 
     def change_projection(self, mode='linear', centerof=None):
         mcenterof = []
         if not self.mode == mode:
-            mcenterof = self.map_temp['centerof'] = (centerof or self.center_of())
+            mcenterof = self.center = (centerof or self.center_of())
         self.scale_x = self.viewx*self.delta
         if mode in ('linear', 'mercator'):
             self.scale_y = self.viewy*self.delta
@@ -322,16 +317,7 @@ class Map():
                                                 )
 
     def convert_coords(self, coords):
-        # interpolate coords for Globe projection
-        _coords = []
-        if self.is_spherical():
-            for i in range(len(coords) - 1):
-                _coords += self.interpolation(coords[i:i+2])
-        if not _coords:
-            _coords = coords
-        coords = self.to_points(_coords, doscale=1)
-        
-        return coords
+        return self.to_points(coords, doscale=1)
         
     def draw_map(self, coords):
         points = self.convert_coords(coords)
@@ -370,25 +356,15 @@ class Map():
     def center_of(self):
         """Return centerof [[x,y]] (in degrees)."""
         if self.is_spherical():
-            return self.map_temp.get('centerof', [[0,0]])
+            print(self.center)
+            return self.center or [[0, 0]]
         else:
             return self.from_points(self.view_center())
 
-    def sizeOf(self):
-        """Return viewport size as [width, height] disregarding scale (in points)."""
-        return [self.scale_x, self.scale_y]
-
-    def viewsizeOf(self):
-        """Return rect. of visible area (in points)."""
-        left, right = [self.scale_x * self.scale * x for x in self.cvs.xview()]
-        top, bottom = [self.scale_y * self.scale * y for y in self.cvs.yview()]
-        return [left, top, right, bottom]
-
     def view_center(self):
-        """Return center of visible area (in points)."""
-        rect = self.viewsizeOf()
-        return [ (rect[0] + rect[2]) / 2.0,
-                 (rect[1] + rect[3]) / 2.0 ]
+        cx = (self.scale_x*self.scale*x for x in self.cvs.xview())
+        cy = (self.scale_y*self.scale*y for y in self.cvs.yview())
+        return (sum(cx)/2, sum(cy)/2)
 
     def to_points(self, coords=[], doscale=0):
         points = []
@@ -397,14 +373,13 @@ class Map():
             if self.mode in ('linear', 'mercator'):
                 x = float(x)
                 y = [self.to_mercator(float(y)), float(y)][self.mode != 'mercator']
-                x, y = self.rotate_z([x, y], centerof)
                 y = -y
             else:
                 # self.mode is 'globe':
                 tmp = self.to_sphere([x, y])
                 if not tmp:
                     continue
-                x, y = self.rotate_z(tmp)
+                x, y = tmp
             _x = x * self.delta + self.halfX 
             _y = y * self.delta + self.halfY
             if doscale:
@@ -413,7 +388,7 @@ class Map():
                 points += [_x, _y]
         return points
 
-    def from_points(self, points=[], dorotatez=1, dosphere=0):
+    def from_points(self, points=[], dosphere=0):
         b, coords = 0, []
         center_x, center_y = self.view_center()
         for point in points:
@@ -422,40 +397,33 @@ class Map():
             else:
                 y, center_y = [-(point / self.scale - self.halfY) / self.delta for point in [point, center_y]]
                 if self.is_spherical() and dosphere:
-                    if dorotatez:
-                        x, y = self.rotate_z([x, y])
                     _coords = self.from_sphere([x, y])
                     if _coords:
                         coords += [_coords]
                 else:
-                    if dorotatez:
-                        x, y = self.rotate_z([x, y], [[center_x, center_y]], reverse=1)
                     y = [self.from_mercator(y), y][self.mode != 'mercator']
                     coords += [[x, y]]
             b = not b
         return coords
 
-    def to_mercator(self, y, ylimit=None):
+    def to_mercator(self, y):
         # latitude in mercator projection
-
-        if not ylimit:
-            ylimit = self.ylimit
-        if abs(y) > ylimit:
-            return self.to_mercator([-1, 1][y > 0] * ylimit)
-        else:
-            return degrees(log(tan(radians(y) / 2.0 + atan(1))))
+        if abs(y) > 84: 
+            y = 84*np.sign(y)
+        return degrees(log(tan(radians(y)/2 + atan(1))))
 
     def from_mercator(self, y):
         # latitude from mercator projection
         return degrees(2*(atan(e**(radians(y))) - atan(1)))
 
     def to_sphere(self, coords):
-        self.map_temp['centerof'] = centerof = self.map_temp.get('centerof', [[0,0]])
-        x, y, center_x, center_y = [radians(float(x)) for x in coords + centerof[0]]
-        center_x += pi
-        if (sin(y) * sin(center_y) - cos(y) * cos(center_y) * cos(center_x - x)) > 0:
-            return [ degrees(cos(y) * sin(center_x - x)),
-                     degrees(-sin(y) * cos(center_y) - sin(center_y) * cos(y) * cos(center_x - x)) ]
+        x, y, cx, cy = [radians(float(x)) for x in coords + self.center[0]]
+        cx += pi
+        if sin(y) * sin(cy) - cos(y)*cos(cy)*cos(cx - x) > 0:
+            return [
+                    degrees(cos(y)*sin(cx - x)),
+                    degrees(-sin(y)*cos(cy) - sin(cy)*cos(y)*cos(cx - x))
+                    ]
 
     def from_sphere(self, coords):
         # internal
@@ -463,9 +431,8 @@ class Map():
         adjust_lon = lambda x: [(x - ([1, -1][x < 0] * 2.0 * pi)), x][abs( x ) < pi]
         EPSLN = 1.0e-10
         # args
-        centerof = self.map_temp.get('centerof', [[0,0]])
+        centerof = self.center or [[0, 0]]
         x, y, center_x, center_y = [radians(float(x)) for x in coords + centerof[0]]
-        #a = 1
         rh = sqrt(x * x + y * y) + EPSLN
         if rh > 1:
             return []
@@ -489,41 +456,8 @@ class Map():
         y = degrees(lat)
         return [x, y]
 
-    def rotate_z(self, coords, centerof=[[0,0]], reverse=0):
-        roll = self.map_temp.get('z_angle', 0)
-        if roll:
-            x, y, center_x, center_y = coords + centerof[0]
-            roll = radians(roll)
-            if reverse: 
-                roll = -roll
-            r = sqrt((center_x - x)*(center_x - x) + (y - center_y) * (y - center_y))
-            if r:
-                a = acos((center_x - x) / r)
-                if ( y < center_y ): a = 2.0 * pi - a
-                coords = [ center_x - r * cos(roll + a),
-                           center_y + r * sin(roll + a) ]
-        return coords
-
     def distance(self, coords):
-        """Return the length of the great circle between two points (in km).
-        COORDS points list [[x,y],[x1,y1]] (in degrees)."""
+        # length of the great circle between two points (km)
         x, y, x1, y1 = [radians(x) for x in coords[0] + coords[1]]
         return 6378.136 * acos(cos(y)*cos(y1)*cos(x - x1) + sin(y)*sin(y1))
 
-    def interpolation(self, coords, scalestep=500):
-        """Return list of coords as interpol. of two points [[x,y],[x1,y1]].
-        COORDS points list [[x,y],[x1,y1]] (in degrees).
-        SCALESTEP step (in km)."""
-        x, y, x1, y1 = coords[0] + coords[1]
-        scalestep = int(self.distance(coords) / scalestep)
-        if not scalestep:
-            return coords
-        scale_dx = (x1 - x) / scalestep
-        scale_dy = (y1 - y) / scalestep
-        _x, _y = x, y
-        interpol_pts = [[_x, _y]]
-        for i in range(scalestep):
-            _x += scale_dx
-            _y += scale_dy
-            interpol_pts += [[_x, _y]]
-        return interpol_pts
