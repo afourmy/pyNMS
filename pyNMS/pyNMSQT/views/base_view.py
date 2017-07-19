ce9799012352474823f1069cab7e75f5ef3648e0
minto <minto@hotmail.fr>
2017-07-19 16:59:24 +0200

    pyNMS QT part 4

--- pyNMS/pyNMSQT/views/base_view.py
+++ pyNMS/pyNMSQT/views/base_view.py
@@ -10,12 +10,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
 class BaseView(QGraphicsView):
-    
-    projections = {
-    'mercator': pyproj.Proj(init='epsg:3395'),
-    'spherical': pyproj.Proj('+proj=ortho +lon_0=28 +lat_0=47')
-    }
-    
+        
     def __init__(self, name, controller):
         super(BaseView, self).__init__(controller)
         self.name = name
@@ -26,14 +21,11 @@ class BaseView(QGraphicsView):
         self.setScene(self.scene)
         self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
         self.setRenderHint(QPainter.Antialiasing)
-        # activate rubberband for selection
-        self.setDragMode(QGraphicsView.RubberBandDrag)
         # call the selection function when the selection changes
         self.scene.selectionChanged.connect(self.change_selection)
         
     def change_selection(self):
-        for item in self.scene.selectedItems():
-            print(item)
+        pass
         
     def wheelEvent(self, event):
         factor = 1.25 if event.angleDelta().y() > 0 else 0.8
@@ -49,34 +41,38 @@ class BaseView(QGraphicsView):
         pos = self.mapToScene(event.pos())
         if event.mimeData().hasFormat('application/x-dnditemdata'):
             item_data = event.mimeData().data('application/x-dnditemdata')
-            dataStream = QtCore.QDataStream(item_data, QtCore.QIODevice.ReadOnly)
-            pixmap, offset = QPixmap(), QtCore.QPoint()
+            dataStream = QDataStream(item_data, QIODevice.ReadOnly)
+            pixmap, offset = QPixmap(), QPoint()
             dataStream >> pixmap >> offset
 
             new_node = GraphicalNode(pixmap, self)
             new_node.setPos(pos - offset)
 
     def mousePressEvent(self, event):
+        # activate rubberband for selection
+        # by default, the rubberband is active for both clicks, we have to
+        # deactivate it explicitly for the right-click
+        if event.buttons() == Qt.LeftButton:
+            self.setDragMode(QGraphicsView.RubberBandDrag)
+        if event.buttons() == Qt.RightButton:
+            self.setDragMode(QGraphicsView.NoDrag)
         item = self.itemAt(event.pos())
         if type(item) == GraphicalNode and event.buttons() == Qt.RightButton:
             self.start_node = item
             self.start_position = pos = self.mapToScene(event.pos())
-            self.temp_line = QGraphicsLineItem(QtCore.QLineF(pos, pos))
+            self.temp_line = QGraphicsLineItem(QLineF(pos, pos))
             self.temp_line.setZValue(1)
             self.scene.addItem(self.temp_line)
-            
-
         super(BaseView, self).mousePressEvent(event)
             
     def mouseReleaseEvent(self, event):
         item = self.itemAt(event.pos())
-        print(isinstance(item, GraphicalNode), event.buttons() == Qt.RightButton)
-        if (isinstance(item, GraphicalNode) and self.temp_line):
+        if isinstance(item, GraphicalNode) and self.temp_line:
             self.end_node = item
             GraphicalLink(self)
-        self.scene.removeItem(self.temp_line)
-        self.temp_line = None
-        if event.button() == Qt.RightButton:
+            self.scene.removeItem(self.temp_line)
+            self.temp_line = None
+        elif event.button() == Qt.RightButton:
             # here you do not call super hence the selection won't be cleared
             menu = QMenu(self)
             exitAction = QAction('&Exit', self)        
@@ -84,76 +80,66 @@ class BaseView(QGraphicsView):
             exitAction.triggered.connect(lambda: _)
             menu.addAction(exitAction)
             menu.exec_(event.globalPos())
+
         super(BaseView, self).mouseReleaseEvent(event)
 
    ##       for point in (start, end):
     #         text = self.scene.addSimpleText(
     #             '(%d, %d)' % (point.x(), point.y()))
-    #         text.setBrush(QtCore.Qt.red)
+    #         text.setBrush(Qt.red)
     #         text.setPos(point)
     
 
-        # for item in self.scene().selectedItems():
-        #     if isinstance(item, NodeItem):
-        #         item.mouseRelease()
 
-       ##   # If the left  mouse button is not still pressed TOGETHER with the SHIFT key and neither is the middle button
-        # # this means the user is no longer trying to drag the view
-        # if self._dragging and not (event.buttons() == QtCore.Qt.LeftButton and event.modifiers() == QtCore.Qt.ShiftModifier) and not event.buttons() & QtCore.Qt.MidButton:
-        #     self._dragging = False
-        #     self.setCursor(QtCore.Qt.ArrowCursor)
-        # else:
-        #     item = self.itemAt(event.pos())
-        #     if item is not None and not event.modifiers() & QtCore.Qt.ControlModifier:
-        #         item.setSelected(True)
-        #     super().mouseReleaseEvent(eve
+        #     self.setCursor(Qt.ArrowCursor)
+
             
     def mouseMoveEvent(self, event):
         position = self.mapToScene(event.pos())
         if event.buttons() == Qt.RightButton and self.temp_line:  
-            self.temp_line.setLine(QtCore.QLineF(self.start_position, position))
+            self.temp_line.setLine(QLineF(self.start_position, position))
 
         super(BaseView, self).mouseMoveEvent(event)
-                    
-class Link(QGraphicsLineItem):
-    def __init__(self, qline):
-        QGraphicsLineItem.__init__(self, qline)
-            
+                                
 class GraphicalNode(QGraphicsPixmapItem):
     
     def __init__(self, pixmap, view):
         super().__init__(pixmap)
         self.setFlag(QGraphicsItem.ItemIsSelectable, True)
         self.setFlag(QGraphicsItem.ItemIsMovable, True)
+        self.setOffset(QPointF(-self.boundingRect().width()/2, -self.boundingRect().height()/2))
         self.setZValue(3)
         self.view = view
         self.node = self.view.network.nf()
         self.view.scene.addItem(self)
+        self.setCursor(QCursor(Qt.PointingHandCursor))
         
-    def mousePressEvent(self, event):
-        super(GraphicalNode, self).mousePressEvent(event)
-    #     
-    # def mouseReleaseEvent(self, event):
-    #     super(GraphicalNode, self).mouseReleaseEvent(event)
+    def mouseMoveEvent(self, event):
+        for item in self.view.scene.selectedItems():
+            if isinstance(item, GraphicalNode):
+                for link in self.view.network.attached_links(item.node):
+                    link.glink.update_position()
+        super(GraphicalNode, self).mouseMoveEvent(event)
         
 class GraphicalLink(QGraphicsLineItem):
     
     def __init__(self, view):
+        super(GraphicalLink, self).__init__()
         # source and destination graphic nodes
         self.destination = view.end_node
         self.source = view.start_node
-        print(self.source, self.destination)
-        
-        # # start / end positions of the link
-        start_position = self.source.pos()
-        end_position = self.destination.pos()
-
-        super(GraphicalLink, self).__init__(QtCore.QLineF(start_position, end_position))
         self.setZValue(5)
+        self.update_position()
         view.scene.addItem(self)
-        
+
         self.link = view.network.lf(
                                     source = self.source.node, 
                                     destination = self.destination.node
                                     )
-        print(self.source, self.destination)
+        self.link.glink = self
+        
+    def update_position(self):
+        # # start / end positions of the link
+        start_position = self.source.pos()
+        end_position = self.destination.pos()
+        self.setLine(QLineF(start_position, end_position))
