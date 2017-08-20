f515fe39242f7ee40dd5f5d8dd68ec2a07b03d48
minto <minto@hotmail.fr>
2017-08-20 10:32:14 +0200

    pyNMS QT part 99

--- pyNMS/test.py
+++ pyNMS/test.py
@@ -138,233 +138,233 @@ class TestMST(unittest.TestCase):
         mst_costs = set(map(lambda plink: plink.costSD, mst))
         self.assertEqual(mst_costs, {1, 2, 4})
         
-# class TestSP(unittest.TestCase):
-#     
-#     results = (
-#     ['ethernet link1', 'ethernet link3', 'ethernet link5'], 
-#     ['ethernet link1', 'ethernet link7'],
-#     ['ethernet link1', 'ethernet link3']
-#     )
-#  
-#     @start_pyNMS_and_import_project('test_SP.xls')
-#     def setUp(self):
-#         get_node = lambda node_name: self.nk.pn['node'][self.nk.name_to_id[node_name]]
-#         self.route9 = (get_node('node0'), get_node('node4'))
-#         self.route10 = (get_node('node0'), get_node('node5'))
-#         self.route11 = (get_node('node0'), get_node('node3'))
-#  
-#     def tearDown(self):
-#         self.app.quit()
-#  
-#     def test_A_star(self):
-#         for i, r in enumerate((self.route9, self.route10, self.route11)):
-#             _, path = self.nk.A_star(r[0], r[1])
-#             self.assertEqual(list(map(str, path)), self.results[i])
-#         
-#     def test_bellman_ford(self):
-#         for i, r in enumerate((self.route9, self.route10, self.route11)):
-#             _, path = self.nk.bellman_ford(r[0], r[1])
-#             self.assertEqual(list(map(str, path)), self.results[i])
-#         
-#     def test_floyd_warshall(self):
-#         cost_plink = lambda plink: plink.costSD
-#         all_length = self.nk.floyd_warshall()
-#         for i, r in enumerate((self.route9, self.route10, self.route11)):
-#             path_length = all_length[r[0]][r[1]]
-#             _, path = self.nk.A_star(r[0], r[1])
-#             self.assertEqual(sum(map(cost_plink, path)), path_length)
-#             
-#     def test_LP(self):
-#         for i, r in enumerate((self.route9, self.route10, self.route11)):
-#             path = self.nk.LP_SP_formulation(r[0], r[1])
-#             self.assertEqual(list(map(str, path)), self.results[i])
-            
-# class TestMCF(unittest.TestCase):
-#     
-#     results = (
-#     ('ethernet link1', 5),
-#     ('ethernet link2', 7),
-#     ('ethernet link3', 3),
-#     ('ethernet link4', 10),
-#     ('ethernet link5', 2)
-#     )
-#     
-#     @start_pyNMS_and_import_project('test_mcf.xls')
-#     def setUp(self):
-#         source = self.nk.pn['node'][self.nk.name_to_id['node1']]
-#         target = self.nk.pn['node'][self.nk.name_to_id['node4']]
-#         self.nk.LP_MCF_formulation(source, target, 12)
-#  
-#     def tearDown(self):
-#         self.app.quit()
-#  
-#     def test_MCF(self):
-#         for plink_name, flow in self.results:
-#             plink = self.nk.pn['plink'][self.nk.name_to_id[plink_name]]
-#             self.assertEqual(plink.flowSD, flow)
+class TestSP(unittest.TestCase):
+    
+    results = (
+    ['ethernet link1', 'ethernet link3', 'ethernet link5'], 
+    ['ethernet link1', 'ethernet link7'],
+    ['ethernet link1', 'ethernet link3']
+    )
+ 
+    @start_pyNMS_and_import_project('test_SP.xls')
+    def setUp(self):
+        get_node = lambda node_name: self.nk.pn['node'][self.nk.name_to_id[node_name]]
+        self.route9 = (get_node('node0'), get_node('node4'))
+        self.route10 = (get_node('node0'), get_node('node5'))
+        self.route11 = (get_node('node0'), get_node('node3'))
+ 
+    def tearDown(self):
+        self.app.quit()
+ 
+    def test_A_star(self):
+        for i, r in enumerate((self.route9, self.route10, self.route11)):
+            _, path = self.nk.A_star(r[0], r[1])
+            self.assertEqual(list(map(str, path)), self.results[i])
         
-# class TestISIS(unittest.TestCase):
-#     
-#     results = (
-#     ('routed traffic link16', {
-#     'ethernet link1', 
-#     'ethernet link2', 
-#     'ethernet link6', 
-#     'ethernet link7',
-#     'router5',
-#     'router4',
-#     'router3',
-#     'router2',
-#     'router6'
-#     }),
-#     
-#     ('routed traffic link15', {
-#     'ethernet link1', 
-#     'ethernet link3', 
-#     'ethernet link4',
-#     'ethernet link5',
-#     'ethernet link7',
-#     'router5',
-#     'router0',
-#     'router1',
-#     'router4',
-#     'router2',
-#     'router6'
-#     }))
-#  
-#     @start_pyNMS_and_import_project('test_ISIS.xls')
-#     def setUp(self):
-#         self.ct.routing_panel.checkboxes[2].setChecked(False)
-#         self.pj.refresh()
-#  
-#     def tearDown(self):
-#         self.app.quit()
-#  
-#     def test_ISIS(self):
-#         self.assertEqual(len(self.nk.pn['traffic']), 2)
-#         for traffic, path in self.results:
-#             # we retrieve the actual route from its name in pn
-#             traffic_link = self.nk.pn['traffic'][self.nk.name_to_id[traffic]]
-#             # we check that the path is conform to IS-IS protocol
-#             self.assertEqual(set(map(str, traffic_link.path)), path)
-#             
-# class TestOSPF(unittest.TestCase):
-#     
-#     results = (
-#     ('routed traffic0', {
-#     'ethernet link1', 
-#     'ethernet link2', 
-#     'ethernet link9', 
-#     'ethernet link10', 
-#     'ethernet link11', 
-#     'ethernet link12',
-#     'ethernet link13',
-#     'router9',
-#     'router7',
-#     'router5',
-#     'router4',
-#     'router6',
-#     'router3',
-#     'router1',
-#     'router0'
-#     }),
-#     
-#     ('routed traffic1', {
-#     'ethernet link1', 
-#     'ethernet link2', 
-#     'ethernet link9', 
-#     'ethernet link8',  
-#     'ethernet link12',
-#     'ethernet link13',
-#     'router9',
-#     'router7',
-#     'router5',
-#     'router4',
-#     'router3',
-#     'router1',
-#     'router0'
-#     }))
-#  
-#     @start_pyNMS_and_import_project('test_ospf.xls')
-#     def setUp(self):
-#         # untick the automatic allocation of IP addresses and names for interfaces
-#         self.ct.routing_panel.checkboxes[2].setChecked(False)
-#         self.pj.refresh()
-#  
-#     def tearDown(self):
-#         self.app.quit()
-#  
-#     def test_OSPF(self):
-#         self.assertEqual(len(self.nk.pn['traffic']), 2)
-#         for traffic_link, path in self.results:
-#             # we retrieve the actual route from its name in pn
-#             traffic_link = self.nk.pn['traffic'][self.nk.name_to_id[traffic_link]]
-#             # we check that the path is conform to OSPF protocol
-#             self.assertEqual(set(map(str, traffic_link.path)), path)
-#             
-# class TestCSPF(unittest.TestCase):
-#     
-#     results = (
-#     ['plink13', 'plink3', 'plink5', 'plink8', 'plink9', 'plink11'],
-#     ['plink14', 'plink15', 'plink15', 'plink14', 'plink13', 'plink3', 'plink5', 
-#     'plink8', 'plink9', 'plink11'],
-#     ['plink13', 'plink3', 'plink3', 'plink13', 'plink14', 'plink15', 'plink15', 
-#     'plink14', 'plink13', 'plink3', 'plink5', 'plink8', 'plink9', 'plink11'],
-#     ['plink14', 'plink15', 'plink1', 'plink4', 'plink3', 'plink5', 'plink8', 
-#     'plink9', 'plink11'],
-#     ['plink14', 'plink15', 'plink2', 'plink5', 'plink8', 'plink9', 'plink11'],
-#     []
-#     )
-#  
-#     @start_pyNMS_and_import_project('test_cspf.xls')
-#     def setUp(self):
-#         pass
-#  
-#     def tearDown(self):
-#         self.app.quit()
-#  
-#     def test_CSPF(self):
-#         node1 = self.nk.nf(name='node1')
-#         node2 = self.nk.nf(name='node2')
-#         node3 = self.nk.nf(name='node3')
-#         node4 = self.nk.nf(name='node4')
-#         node6 = self.nk.nf(name='node6')
-#         node7 = self.nk.nf(name='node7')
-#         # plink between node4 and node6
-#         plink13 = self.nk.lf(name='plink13')
-#         # plink between node2 and node5
-#         plink15 = self.nk.lf(name='plink15')
-#         
-#         _, path = self.nk.A_star(node6, node7)
-#         self.assertEqual(list(map(str, path)), self.results[0])
-#         _, path = self.nk.A_star(node6, node7, 
-#                                                     path_constraints=[node2])
-#         self.assertEqual(list(map(str, path)), self.results[1])
-#         _, path = self.nk.A_star(node6, node7, 
-#                                             path_constraints=[node3, node2])
-#         self.assertEqual(list(map(str, path)), self.results[2])                  
-#         _, path = self.nk.A_star(node6, node7, 
-#                                                     excluded_plinks={plink13})
-#         self.assertEqual(list(map(str, path)), self.results[3])
-#         _, path = self.nk.A_star(node6, node7, 
-#                             excluded_plinks={plink13}, excluded_nodes={node1})
-#         self.assertEqual(list(map(str, path)), self.results[4])
-#         _, path = self.nk.A_star(node6, node7, 
-#                             excluded_plinks={plink15}, excluded_nodes={node4})
-#         self.assertEqual(list(map(str, path)), self.results[5])
-#         
-# class TestRWA(unittest.TestCase):
-#      
-#     @start_pyNMS_and_import_project('test_RWA.xls')
-#     def setUp(self):
-#         pass
-#  
-#     def tearDown(self):
-#         self.ct.destroy()
-#         
-#     def test_RWA(self):
-#         project_new_graph = self.nk.RWA_graph_transformation()
-#         self.assertEqual(project_new_graph.network.LP_RWA_formulation(), 3)
+    def test_bellman_ford(self):
+        for i, r in enumerate((self.route9, self.route10, self.route11)):
+            _, path = self.nk.bellman_ford(r[0], r[1])
+            self.assertEqual(list(map(str, path)), self.results[i])
+        
+    def test_floyd_warshall(self):
+        cost_plink = lambda plink: plink.costSD
+        all_length = self.nk.floyd_warshall()
+        for i, r in enumerate((self.route9, self.route10, self.route11)):
+            path_length = all_length[r[0]][r[1]]
+            _, path = self.nk.A_star(r[0], r[1])
+            self.assertEqual(sum(map(cost_plink, path)), path_length)
+            
+    def test_LP(self):
+        for i, r in enumerate((self.route9, self.route10, self.route11)):
+            path = self.nk.LP_SP_formulation(r[0], r[1])
+            self.assertEqual(list(map(str, path)), self.results[i])
+            
+class TestMCF(unittest.TestCase):
+    
+    results = (
+    ('ethernet link1', 5),
+    ('ethernet link2', 7),
+    ('ethernet link3', 3),
+    ('ethernet link4', 10),
+    ('ethernet link5', 2)
+    )
+    
+    @start_pyNMS_and_import_project('test_mcf.xls')
+    def setUp(self):
+        source = self.nk.pn['node'][self.nk.name_to_id['node1']]
+        target = self.nk.pn['node'][self.nk.name_to_id['node4']]
+        self.nk.LP_MCF_formulation(source, target, 12)
+ 
+    def tearDown(self):
+        self.app.quit()
+ 
+    def test_MCF(self):
+        for plink_name, flow in self.results:
+            plink = self.nk.pn['plink'][self.nk.name_to_id[plink_name]]
+            self.assertEqual(plink.flowSD, flow)
+        
+class TestISIS(unittest.TestCase):
+    
+    results = (
+    ('routed traffic link16', {
+    'ethernet link1', 
+    'ethernet link2', 
+    'ethernet link6', 
+    'ethernet link7',
+    'router5',
+    'router4',
+    'router3',
+    'router2',
+    'router6'
+    }),
+    
+    ('routed traffic link15', {
+    'ethernet link1', 
+    'ethernet link3', 
+    'ethernet link4',
+    'ethernet link5',
+    'ethernet link7',
+    'router5',
+    'router0',
+    'router1',
+    'router4',
+    'router2',
+    'router6'
+    }))
+ 
+    @start_pyNMS_and_import_project('test_ISIS.xls')
+    def setUp(self):
+        self.ct.routing_panel.checkboxes[2].setChecked(False)
+        self.pj.refresh()
+ 
+    def tearDown(self):
+        self.app.quit()
+ 
+    def test_ISIS(self):
+        self.assertEqual(len(self.nk.pn['traffic']), 2)
+        for traffic, path in self.results:
+            # we retrieve the actual route from its name in pn
+            traffic_link = self.nk.pn['traffic'][self.nk.name_to_id[traffic]]
+            # we check that the path is conform to IS-IS protocol
+            self.assertEqual(set(map(str, traffic_link.path)), path)
+            
+class TestOSPF(unittest.TestCase):
+    
+    results = (
+    ('routed traffic0', {
+    'ethernet link1', 
+    'ethernet link2', 
+    'ethernet link9', 
+    'ethernet link10', 
+    'ethernet link11', 
+    'ethernet link12',
+    'ethernet link13',
+    'router9',
+    'router7',
+    'router5',
+    'router4',
+    'router6',
+    'router3',
+    'router1',
+    'router0'
+    }),
+    
+    ('routed traffic1', {
+    'ethernet link1', 
+    'ethernet link2', 
+    'ethernet link9', 
+    'ethernet link8',  
+    'ethernet link12',
+    'ethernet link13',
+    'router9',
+    'router7',
+    'router5',
+    'router4',
+    'router3',
+    'router1',
+    'router0'
+    }))
+ 
+    @start_pyNMS_and_import_project('test_ospf.xls')
+    def setUp(self):
+        # untick the automatic allocation of IP addresses and names for interfaces
+        self.ct.routing_panel.checkboxes[2].setChecked(False)
+        self.pj.refresh()
+ 
+    def tearDown(self):
+        self.app.quit()
+ 
+    def test_OSPF(self):
+        self.assertEqual(len(self.nk.pn['traffic']), 2)
+        for traffic_link, path in self.results:
+            # we retrieve the actual route from its name in pn
+            traffic_link = self.nk.pn['traffic'][self.nk.name_to_id[traffic_link]]
+            # we check that the path is conform to OSPF protocol
+            self.assertEqual(set(map(str, traffic_link.path)), path)
+            
+class TestCSPF(unittest.TestCase):
+    
+    results = (
+    ['plink13', 'plink3', 'plink5', 'plink8', 'plink9', 'plink11'],
+    ['plink14', 'plink15', 'plink15', 'plink14', 'plink13', 'plink3', 'plink5', 
+    'plink8', 'plink9', 'plink11'],
+    ['plink13', 'plink3', 'plink3', 'plink13', 'plink14', 'plink15', 'plink15', 
+    'plink14', 'plink13', 'plink3', 'plink5', 'plink8', 'plink9', 'plink11'],
+    ['plink14', 'plink15', 'plink1', 'plink4', 'plink3', 'plink5', 'plink8', 
+    'plink9', 'plink11'],
+    ['plink14', 'plink15', 'plink2', 'plink5', 'plink8', 'plink9', 'plink11'],
+    []
+    )
+ 
+    @start_pyNMS_and_import_project('test_cspf.xls')
+    def setUp(self):
+        pass
+ 
+    def tearDown(self):
+        self.app.quit()
+ 
+    def test_CSPF(self):
+        node1 = self.nk.nf(name='node1')
+        node2 = self.nk.nf(name='node2')
+        node3 = self.nk.nf(name='node3')
+        node4 = self.nk.nf(name='node4')
+        node6 = self.nk.nf(name='node6')
+        node7 = self.nk.nf(name='node7')
+        # plink between node4 and node6
+        plink13 = self.nk.lf(name='plink13')
+        # plink between node2 and node5
+        plink15 = self.nk.lf(name='plink15')
+        
+        _, path = self.nk.A_star(node6, node7)
+        self.assertEqual(list(map(str, path)), self.results[0])
+        _, path = self.nk.A_star(node6, node7, 
+                                                    path_constraints=[node2])
+        self.assertEqual(list(map(str, path)), self.results[1])
+        _, path = self.nk.A_star(node6, node7, 
+                                            path_constraints=[node3, node2])
+        self.assertEqual(list(map(str, path)), self.results[2])                  
+        _, path = self.nk.A_star(node6, node7, 
+                                                    excluded_plinks={plink13})
+        self.assertEqual(list(map(str, path)), self.results[3])
+        _, path = self.nk.A_star(node6, node7, 
+                            excluded_plinks={plink13}, excluded_nodes={node1})
+        self.assertEqual(list(map(str, path)), self.results[4])
+        _, path = self.nk.A_star(node6, node7, 
+                            excluded_plinks={plink15}, excluded_nodes={node4})
+        self.assertEqual(list(map(str, path)), self.results[5])
+        
+class TestRWA(unittest.TestCase):
+     
+    @start_pyNMS_and_import_project('test_RWA.xls')
+    def setUp(self):
+        pass
+ 
+    def tearDown(self):
+        self.app.quit()
+        
+    def test_RWA(self):
+        project_new_graph = self.nk.RWA_graph_transformation()
+        self.assertEqual(project_new_graph.network.LP_RWA_formulation(), 3)
 
 ## Graph generation and IGP simulation
 
